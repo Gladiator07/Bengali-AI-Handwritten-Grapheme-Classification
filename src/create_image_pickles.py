f9f281c31ed9ede3cb0f152db9f92c06bdddb886
Gladiator07 <atharvaaingle@gmail.com>
2021-02-15 18:02:35 +0000

    fixed path

--- src/create_image_pickles.py
+++ src/create_image_pickles.py
@@ -9,8 +9,8 @@ if __name__=="__main__":
 
     for f in files:
         df = pd.read_parquet(f)
-        image_ids = df.image_ids.values
+        image_ids = df.image_id.values
         df = df.drop("image_id", axis=1)
         image_array = df.values
         for j, img_id in tqdm(enumerate(image_ids), total=len(image_ids)):
-            joblib.dump(image_array[j, :], f"../root/input/image_pickles/{img_id}.pkl")
+            joblib.dump(image_array[j, :], f"/root/input/image_pickles/{img_id}.pkl")
