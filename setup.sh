1c5d20bd6a3be89d5eaabdb5612c8d8e7192dc14
Gladiator07 <atharvaaingle@gmail.com>
2021-02-15 17:39:04 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 echo "This script will set your kaggle api-key to Kaggle api, download data, create folds and convert parquet files to pickle format"
 
-
 echo "Installing dependencies"
 pip3 install iterative-stratification # for MultiLabelStratifiedKfold
 pip3 install kaggle --upgrade
@@ -14,7 +13,7 @@ kaggle_api_key_path='/content/drive/MyDrive/Kaggle/kaggle.json'
 # This snippet will install kaggle api and connect your api-key to it
 mkdir -p ~/.kaggle
 echo "Setting up your Kaggle key to API..."
-cp kaggle_api_key_path ~/.kaggle/
+cp $kaggle_api_key_path ~/.kaggle/
 cat ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
 echo "Kaggle API Key successfully linked !!!"
@@ -22,9 +21,9 @@ echo "Kaggle API Key successfully linked !!!"
 # This snippet will download the data in specified folder
 
 # Specify the data path here
-data_path = '~/.'
+data_path='/root/'
 echo "Downloading Data at specified directory..."
-cd data_path
+cd ~/.
 mkdir input
 cd input/
 kaggle competitions download -c bengaliai-cv19
@@ -37,13 +36,15 @@ echo "Data downloaded and unzipped successfully..."
 
 echo "Creating train folds"
 
-python3 /content/Bengali-AI-Handwritten-Grapheme-Classification/src/create_folds.py
+folds_script='/content/Bengali-AI-Handwritten-Grapheme-Classification/src/create_folds.py'
+python3 $folds_script
 
 mkdir image_pickles
 cd image_pickles/
 
+image_pickles='/content/Bengali-AI-Handwritten-Grapheme-Classification/src/create_image_pickles.py'
 echo "Converting parquet data to pickle format"
 
-python3 /content/Bengali-AI-Handwritten-Grapheme-Classification/src/create_image_pickles.py
+python3 $image_pickles
 
 echo "Setup completed successfully"
