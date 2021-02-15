c5e078c531d553f071f1d1060001b350b18e8a0b
Gladiator07 <atharvaaingle@gmail.com>
2021-02-15 17:29:41 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "This script will set your kaggle api-key to Kaggle api, download data, create folds and convert parquet files to pickle format"
+
+
 echo "Installing dependencies"
 pip3 install iterative-stratification # for MultiLabelStratifiedKfold
 pip3 install kaggle --upgrade
@@ -12,14 +14,17 @@ kaggle_api_key_path='/content/drive/MyDrive/Kaggle/kaggle.json'
 # This snippet will install kaggle api and connect your api-key to it
 mkdir -p ~/.kaggle
 echo "Setting up your Kaggle key to API..."
-cp /content/drive/MyDrive/Kaggle/kaggle.json ~/.kaggle/
+cp kaggle_api_key_path ~/.kaggle/
 cat ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
 echo "Kaggle API Key successfully linked !!!"
 
-# This snippet will download the data in root folder
-echo "Downloading Data at root directory..."
-cd ~/.
+# This snippet will download the data in specified folder
+
+# Specify the data path here
+data_path = '~/.'
+echo "Downloading Data at specified directory..."
+cd data_path
 mkdir input
 cd input/
 kaggle competitions download -c bengaliai-cv19
