686e84b429475e5251a0c8e80090f7bd3b1caa5b
Gladiator07 <atharvaaingle@gmail.com>
2021-02-15 10:28:23 +0000

    bash script to download data

---
+++ get_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+echo "This script will set your kaggle api-key to Kaggle api, download data
+and arrange it in train, val, test directories"
+
+
+# Put your Kaggle api key path here
+echo "Fetching your Kaggle API Key"
+kaggle_api_key_path='/content/drive/MyDrive/Kaggle/kaggle.json'
+
+# This snippet will install kaggle api and connect your api-key to it
+echo "Installing Kaggle API..."
+pip3 install -q kaggle
+mkdir -p ~/.kaggle
+echo "Setting up your Kaggle key to API..."
+cp /content/drive/MyDrive/Kaggle/kaggle.json ~/.kaggle/
+cat ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+echo "Kaggle API Key successfully linked !!!"
+
+# This snippet will download the data in root folder
+echo "Downloading Data at root directory..."
+cd ~/.
+mkdir input
+cd input/
+kaggle competitions download -c bengaliai-cv19
+echo "Unzipping *.parquet files"
+unzip \*.zip
+
+echo "Deleting *.zip files"
+rm \*.zip
+echo "Data downloaded and unzipped successfully..."
