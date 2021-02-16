10a04d6b4d5123b0d899510b635e8ae24fde5b37
Gladiator07 <atharvaaingle@gmail.com>
2021-02-16 14:46:29 +0000

    resnet34 model

---
+++ src/models.py
@@ -0,0 +1,25 @@
+import pretrainedmodels
+import torch.nn as nn
+from torch.nn import functional as F
+
+class ResNet34(nn.Module):
+    def __init__(self):
+        super(ResNet34, self, pretrained).__init__()
+        if pretrained is True:
+            self.model = pretrainedmodels.__dict__["resnet34"](pretrained="imagenet")
+        else:
+            self.model = pretrainedmodels.__dict__["resnet34"](pretrained=None)
+        
+        self.l0 = nn.Linear(512, 168)
+        self.l1 = nn.Linear(512, 11)
+        self.l2 = nn.Linear(512, 7)
+    
+    def forward(self, x):
+        bs, _, _, _ = x.shape
+        x = self.model.features(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(bs, -1)
+        l0 = self.l0(x)
+        l1 = self.l1(x)
+        l2 = self.l2(x)
+    
+        return l0, l1, l2
